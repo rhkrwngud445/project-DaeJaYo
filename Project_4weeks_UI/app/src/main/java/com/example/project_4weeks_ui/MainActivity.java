07610520734920b2fea1dfe6cc1acbfddcf47a81
sjw989 <a980917a@naver.com>
2021-07-30 20:46:29 +0900

    Intent, back

--- Project_4weeks_UI/app/src/main/java/com/example/project_4weeks_ui/MainActivity.java
+++ Project_4weeks_UI/app/src/main/java/com/example/project_4weeks_ui/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.project_4weeks_ui;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.gson.JsonArray;
@@ -24,37 +29,28 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_SELECT_MENU = 101;
     private Retrofit mRetrofit;
-    TextView textView1;
-    TextView textView2;
-    TextView textView3;
-    TextView textView4;
-    TextView textView5;
-    TextView textView6;
-    TextView textView7;
-    TextView textView8;
     public static String baseURL = "http://211.237.50.150:7080/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.receipe);
-        textView1 = findViewById(R.id.textView1);
-        textView1.setMovementMethod(new ScrollingMovementMethod());
-        textView2 = findViewById(R.id.textView2);
-        textView2.setMovementMethod(new ScrollingMovementMethod());
-        textView3 = findViewById(R.id.textView3);
-        textView3.setMovementMethod(new ScrollingMovementMethod());
-        textView4 = findViewById(R.id.textView4);
-        textView4.setMovementMethod(new ScrollingMovementMethod());
-        textView5 = findViewById(R.id.textView5);
-        textView5.setMovementMethod(new ScrollingMovementMethod());
-        textView6 = findViewById(R.id.textView6);
-        textView6.setMovementMethod(new ScrollingMovementMethod());
-        textView7 = findViewById(R.id.textView7);
-        textView7.setMovementMethod(new ScrollingMovementMethod());
-        textView8 = findViewById(R.id.textView8);
-        textView8.setMovementMethod(new ScrollingMovementMethod());
+        setContentView(R.layout.activity_main);
+
+        ImageButton imageButton = findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), select_menu.class);
+                startActivityForResult(intent, REQUEST_CODE_SELECT_MENU);
+            }
+        });
+
+
+
+
+
 
         mRetrofit = new Retrofit.Builder()
                 .baseUrl(baseURL)
@@ -78,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
 
 interface RecipeInterface {
@@ -111,4 +106,3 @@ class DataClass {
                 '}';
     }
 }
-
