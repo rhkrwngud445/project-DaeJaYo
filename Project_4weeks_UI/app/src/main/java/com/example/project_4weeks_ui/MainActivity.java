d5b651a8f1c95476b4083352d654247b896c7068
sjw989 <a980917a@naver.com>
2021-08-23 00:53:13 +0900

    design complete | thread issue

--- Project_4weeks_UI/app/src/main/java/com/example/project_4weeks_ui/MainActivity.java
+++ Project_4weeks_UI/app/src/main/java/com/example/project_4weeks_ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.project_4weeks_ui;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -59,40 +60,45 @@ public class MainActivity extends AppCompatActivity {
 
         // 검색기능
         EditText etv_searching_word = (EditText) findViewById(R.id.etv_enter_searchingWord);
+        etv_searching_word.getText().clear();
         ImageButton btn_search = (ImageButton) findViewById(R.id.btn_search); // 검색버튼
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 search_word = etv_searching_word.getText().toString(); // 검색한 단어
-                database = FirebaseDatabase.getInstance();
-                databaseReference = database.getReference();
-                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot snapshot) {
-                        search_menu.clear();
-                        for (DataSnapshot categorySnapshot : snapshot.getChildren()) {
-                            for (DataSnapshot menuSnapshot : categorySnapshot.getChildren()) {
-                                String name = menuSnapshot.child("name").getValue().toString();
-                                if (name.contains(search_word)) {
-                                    Menu menu = new Menu();
-                                    menu.set_name(menuSnapshot.child("name").getValue().toString());
-                                    menu.set_img_URL(menuSnapshot.child("img").getValue().toString());
-                                    menu.set_info1(menuSnapshot.child("info").child("info1").getValue().toString());
-                                    menu.set_info2(menuSnapshot.child("info").child("info2").getValue().toString());
-                                    menu.set_info3(menuSnapshot.child("info").child("info3").getValue().toString());
-                                    search_menu.add(menu);
+                if(search_word.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "검색어를 입력해주세요 !!" , Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    database = FirebaseDatabase.getInstance();
+                    databaseReference = database.getReference();
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot snapshot) {
+                            search_menu.clear();
+                            for (DataSnapshot categorySnapshot : snapshot.getChildren()) {
+                                for (DataSnapshot menuSnapshot : categorySnapshot.getChildren()) {
+                                    String name = menuSnapshot.child("name").getValue().toString();
+                                    if (name.contains(search_word)) {
+                                        Menu menu = new Menu();
+                                        menu.set_name(menuSnapshot.child("name").getValue().toString());
+                                        menu.set_img_URL(menuSnapshot.child("img").getValue().toString());
+                                        menu.set_info1(menuSnapshot.child("info").child("info1").getValue().toString());
+                                        menu.set_info2(menuSnapshot.child("info").child("info2").getValue().toString());
+                                        menu.set_info3(menuSnapshot.child("info").child("info3").getValue().toString());
+                                        search_menu.add(menu);
+                                    }
                                 }
                             }
+                            Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                            startActivity(intent);
                         }
-                        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Toast.makeText(getApplicationContext(), "검색 실패!!", Toast.LENGTH_SHORT).show();
-                    }
-                });
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(getApplicationContext(), "검색 실패!!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
             }
         });
 
